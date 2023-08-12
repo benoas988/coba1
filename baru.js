d71b6d394b17888c9b68c59b740d2fd5c597003c
BenoAbriyan <128135053+benoas988@users.noreply.github.com>
2023-08-12 23:09:12 +0700

    Add files via upload

---
+++ baru.js
@@ -0,0 +1,30 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+let gettombol = document.getElementById("tentang");
+
+
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var tentang = document.getElementById("tentang");
+var target = document.querySelector(".tab-title");
+
+tentang.addEventListener("click", function(event){
+      event.preventDefault();
+      target.scrollIntoView ({behavior:"smooth"});
+});
+
+document.getElementById("port").addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.href = "baru-p.html";
+});
